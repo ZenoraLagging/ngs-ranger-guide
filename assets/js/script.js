0305a10f49f07b13716a00cc1e52356d06ecd609
Lawrence Lu <lawrencelu18@gmail.com>
2021-06-13 18:05:37 -0700

    added photon arts html

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,6 +77,36 @@ var dictionary = {
   {
       'na': 'Bad Condition Ward Reduction',
       'jp': 'Bad Condition Ward Reduction',
+  },
+  'Raging Shot':
+  {
+      'na':'Razing Shot',
+      'jp':'Raging Shot'
+  },
+  'Homing Dart':
+  {
+      'na':'Homing Darts',
+      'jp':"Homing Dart"
+  },
+  'Blaze Shot':
+  {
+      'na':'Blaze Shot',
+      'jp':'Blaze Shot'
+  },
+  'Fallen Impact':
+  {
+      'na':'Fallen Impact',
+      'jp':'Fallen Impact'
+  },
+  'Fear Eraser':
+  {
+      'na':'Fear Eraser',
+      'jp':'Fear Eraser'
+  },
+  'Multiple Launch':
+  {
+      'na':'Multi-launch',
+      'jp':'Multiple Launch'
   }
 
 };
@@ -107,3 +137,54 @@ catch(err)
 }
 
 translate();
+
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .not('[href="#recommended"]')
+  .not('[href="#optional"]')
+  .not('[href="#situational"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+  $('.collapse').collapse({
+    toggle: false
+  })
+
+  $("#rifle-button").click(function(){
+    $(this).hasClass("active") == false ? $(this).addClass("active") : $(this).removeClass("active");
+  });
+
+  $("#launcher-button").click(function(){
+    $(this).hasClass("active") == false ? $(this).addClass("active") : $(this).removeClass("active");
+  });
